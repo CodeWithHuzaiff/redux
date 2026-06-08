ba8805b07eedc9aa8d1ab340eaf32f424cacf922
Mohammad Huzaif <codewithhuzaiff@gmail.com>
2026-06-08 11:35:58 +0530

    update 4.2

---
+++ redux/src/features/todo/todoSlice.js
@@ -0,0 +1,40 @@
+import { createSlice,nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+    todos : [{
+        id:"abc",
+        task:"demo-task",
+        isDone:false,
+    }],
+};
+
+export const todoSlice = createSlice({
+    name:"todo",
+    initialState,
+    reducers:{
+        addTodo:(state,action)=>{
+            const newTodo={
+                id:nanoid(),
+                task:action.payload,
+                isDone:false,
+            };
+            state.todos.push(newTodo)
+        },
+        deleteTodo:(state,action)=>{
+            state.todos=state.todos.filter((todo)=>{
+                todo.id !== action.payload;
+            })
+        },
+        markAsDOne:(state,action)=>{
+            state.todos=state.todos.map((todo)=>{
+                if(todo.id===action.payload){
+                    isDone=true;
+                }
+            })
+        }
+
+    }
+});
+
+export const {addTodo,deleteTodo,markAsDOne}=todoSlice.actions;
+export default todoSlice.reducer;
